b88b3c2b2e225118e19534f5eef230b1577ce3ee
aryapangestu <aryapangestu02@gmail.com>
2024-09-24 08:31:32 +0700

    Add [M05] Profil Shared Layout BreadcrumbItem dinamis and Add tombol pasien profil ke view pasien
    - Updated `ProfilController.cs` to add new namespace, change role in `ViewBag.Role`, and add breadcrumb list.
    - Modified `Index.cshtml` to set layout based on role, replace `@Html.Partial` with `<partial>`, and add conditional rendering for roles.
    - Updated `TabPasien.cshtml` to replace static content with "Loading..." message and add commented-out AJAX script.
    - Changed `_ProfilLayout.cshtml` to dynamically generate breadcrumb items, replace static side menu with partial view, and simplify main content area.
    - Added `BreadcrumbItem.cs` class to represent breadcrumb items.
    - Added `PasienController.cs` to handle "Pasien" related actions and set up `ViewBag` properties.
    - Added `Index.cshtml` for `PasienController` to set layout and display "DAFTAR PASIEN".
    - Added `_SideMenuProfil.cshtml` to render side menu based on role and include navigation script.

---
+++ HealthCare340B.Web/HealthCare340B.Web/AddOns/BreadcrumbItem.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthCare340B.Web.AddOns
+{
+    public class BreadcrumbItem
+    {
+        public string Name { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public bool IsActive { get; set; } = false;
+    }
+}
--- HealthCare340B.Web/HealthCare340B.Web/Controllers/PasienController.cs
+++ HealthCare340B.Web/HealthCare340B.Web/Controllers/PasienController.cs
@@ -3,20 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HealthCare340B.Web.Controllers
 {
-    public class ProfilController : Controller
+    [Route("Profil/Pasien")]
+    public class PasienController : Controller
     {
         public IActionResult Index()
         {
-            // Mengirim role ke view
-            ViewBag.Title = "Profil";
-
-            //ViewBag.Role = "ROLE_DOKTER";
+            ViewBag.Title = "Daftar Pasien";
             ViewBag.Role = "ROLE_PASIEN";
 
             ViewBag.Breadcrumb = new List<BreadcrumbItem>
             {
                 new BreadcrumbItem { Name = "Beranda", Controller = "Home", Action = "Index" },
-                new BreadcrumbItem { Name = "Profile", IsActive = true }
+                new BreadcrumbItem { Name = "Profile", Controller = "Profil", Action = "Index" },
+                new BreadcrumbItem { Name = "Pasien", IsActive = true }
             };
 
             return View();
