977bce6d3ced241ca42c60de5b343bc350e6d925
aryapangestu <aryapangestu02@gmail.com>
2024-09-26 08:40:01 +0700

    [M05] Profil Shared Layout: ubah flow load layout dan pindah view menggunakan kondisi role dari jquery ke controller
    Refactored ProfileController to handle different roles (`ROLE_DOKTER`, `ROLE_PASIEN`, `ROLE_ADMIN`) with new methods and a switch statement in the `Index` method. Updated `Index.cshtml` to dynamically load content using jQuery's `load` method. Simplified layout logic and removed redundant views. Added `IndexPasienProfile.cshtml` for patient profiles and updated `_DokterProfilLayout.cshtml` and `_ProfilLayout.cshtml` for better formatting and cleanup. Enhanced `_SideMenuProfil.cshtml` for smoother navigation.

--- HealthCare340B.Web/HealthCare340B.Web/Controllers/ProfileController.cs
+++ HealthCare340B.Web/HealthCare340B.Web/Controllers/ProfileController.cs
@@ -14,12 +14,64 @@ namespace HealthCare340B.Web.Controllers
             profile = new ProfileModel(_config, _webHostEnv);
             imageFolder = _config["ImageFolder"];
         }
+
         public async Task<IActionResult> Index()
         {
             // Mengirim role ke view
             ViewBag.Title = "Profil";
             ViewBag.imgFolder = imageFolder;
-            //ViewBag.Role = "ROLE_DOKTER";
+
+            //string Role = "ROLE_DOKTER";
+            string role = "ROLE_DOKTER";
+
+            ViewBag.Breadcrumb = new List<BreadcrumbItem>
+            {
+                new BreadcrumbItem { Name = "Beranda", Controller = "Home", Action = "Index" },
+                new BreadcrumbItem { Name = "Profile", IsActive = true }
+            };
+
+            switch (role)
+            {
+                case "ROLE_PASIEN":
+                    ViewBag.Url = "/Profile/IndexPasienProfile";
+                    break;
+
+                case "ROLE_DOKTER":
+                    ViewBag.Url = "/Profile/TabDokter";
+                    break;
+
+                case "ROLE_ADMIN":
+                    ViewBag.Url = "/Profile/IndexAdminProfile";
+                    break;
+
+                default:
+                    ViewBag.Url = "#";
+                    break;
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> TabDokter()
+        {
+            ViewBag.Title = "Profil";
+            ViewBag.imgFolder = imageFolder;
+            ViewBag.Role = "ROLE_DOKTER";
+
+            ViewBag.Breadcrumb = new List<BreadcrumbItem>
+            {
+                new BreadcrumbItem { Name = "Beranda", Controller = "Home", Action = "Index" },
+                new BreadcrumbItem { Name = "Profile", IsActive = true }
+            };
+
+            VMMDoctor? data = await profile.GetByIdProfilDokter(1);
+            return View(data);
+        }
+
+        public async Task<IActionResult> IndexPasienProfile()
+        {
+            ViewBag.Title = "Profil";
+            ViewBag.imgFolder = imageFolder;
             ViewBag.Role = "ROLE_PASIEN";
 
             ViewBag.Breadcrumb = new List<BreadcrumbItem>
@@ -28,14 +80,9 @@ namespace HealthCare340B.Web.Controllers
                 new BreadcrumbItem { Name = "Profile", IsActive = true }
             };
 
-            if (ViewBag.Role == "ROLE_DOKTER")
-            {
-                VMMDoctor? data = await profile.GetByIdProfilDokter(1);
-                return View(data);
-            }
-
             return View();
         }
+
         [HttpPost]
         public async Task<VMResponse<VMMBiodatum>> EditAsync(VMMBiodatum data)
         {
